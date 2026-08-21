2da4eba2a054e9c99de2b02add927734a8c6fb54
Izyan Khan <izyankhan@MacBookPro.lan>
2026-08-20 17:01:30 -0700

    implement code

--- Project.py
+++ Project.py
@@ -1,24 +1,34 @@
-print("Hello Welcome Please Login below")
-name = input("Name :")
-verified_name = "Izyan"
-while name != verified_name:
-    print("Sorry I dont see your name Please try again")
-    name = input("Name :")
-print("Please Enter your password below")
+print("Hello user this is a place where you can add tasks, delete tasks and view them!")
+print("Login")
+name = input("Username :")
+while name != "Izyan":
+    print("Sorry Invalid username")
+    name = input("Username :")
+print("Welcome back Izyan!")
 password = input("Password :")
-verified_password = "Welcome7643"
-while password != verified_password:
-    print("Incorrect Password")
+while password != "123456789":
+    print("Sorry Invalid Password")
     password = input("Password :")
-print("Welcome![before we continue you have to guess the correct questions]")
-import random
-colors = ["Red","Blue","Green"]
-for i in range(len(colors)):
-    print(i + 1, colors[i])
-result = random.choice(colors)
-answer = input("Enter your answer here :")
-while answer != random.choice:
-    print("Sorry That is incorrect please try again")
-    answer = input("Enter your answer here :")
-print("That is correct!")
-print(result)
+print("Login Successful!")
+print("Here are your tasks for today :")
+tasks = ["Finish Book report", "Code a project", "Clean the house"]
+for i in range(len(tasks)):
+    print(i + 1, tasks[i])
+option1 = input("Would you like to add something or delete something from your list? say yes to add, delete to delete a task and no to keep things as they are :")
+if option1.lower() == "yes":
+   new_task = input("Enter your new task :")
+   tasks.append(new_task)
+   print("Here are your updated tasks for today :")
+for i in range(len(tasks)):
+        print(i + 1, tasks[i])
+if option1.lower() == "no":
+    print("Alright no changes " \
+    "made.")
+if option1 == "delete":
+     delete_task = input("Enter the task you want to delete :")
+     tasks.remove(delete_task)
+     print("Here are your updated tasks for today :")
+for i in range(len(tasks)):
+        print(i + 1, tasks[i])
+        
+    
